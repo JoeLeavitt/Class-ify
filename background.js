4a899bf5187df75a66419b7973b850e1426b5d43
Joe <joeleav1@gmail.com>
2015-11-06 21:41:00 -0500

    add listener for http requests

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
+/******************************
+ * Created by: Joseph Leavitt *
+ * Last Edited: 11/06/15      *
+ ******************************/
 
 
+/* Fires injectScript.js when extension button is pressed */
 chrome.browserAction.onClicked.addListener(function (tab)
 {
 	chrome.tabs.executeScript(tab.id, {
@@ -8,3 +13,42 @@ chrome.browserAction.onClicked.addListener(function (tab)
 		console.log("Script Executed");
 	});
 });
+
+/* XHTTP Request */
+chrome.runtime.onMessage.addListener(function (request, sender, callback)
+{
+	if(request.action = "xhttp")
+	{
+		var xhttpRequest = new XMLHttpRequest();
+		var method = request.method ? request.method.toUpperCase() : 'GET';
+		var professorName = request.professorName;
+		var url = request.url;
+
+		var data = {
+			responseXML: "",
+			professorName: professorName,
+			url: url
+		};
+
+		xhttp.onload = function()
+		{
+			data.responseXML = this.responseText;
+			callback(data);
+		};
+
+		xhttp.onerror = function()
+		{
+			callback();
+		};
+
+		xhttp.open(method, request.url, true);
+
+		if(method == 'POST')
+			xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+		xhttp.send();
+
+		return true;
+	}
+});
+
