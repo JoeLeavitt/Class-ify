0c32a62211b39f978494b422c4dcf4e504aa2237
Joe <joeleav1@gmail.com>
2016-01-16 00:11:55 -0500

    get cross server xhttp request working

--- background.js
+++ background.js
@@ -1,53 +1,58 @@
 /******************************
  * Created by: Joseph Leavitt *
- * Last Edited: 11/06/15      *
+ * Last Edited: 1/16/16       *
  ******************************/
 
 /* Fires injectScript.js when extension button is pressed */
 chrome.browserAction.onClicked.addListener(function (tab)
 {
 	chrome.tabs.executeScript(tab.id, {
-		"file": "injectScript.js"
+        "file": "injectScript.js"
 	}, function () {
-		console.log("Script Executed");
+        console.log("Script Executed");
 	});
 });
 
 /* XHTTP Request */
-chrome.runtime.onMessage.addListener(function (request, sender, callback)
-{
-	if(request.action = "xhttp")
-	{
-		var xhttpRequest = new XMLHttpRequest();
-		var method = request.method ? request.method.toUpperCase() : 'GET';
-		var professorName = request.professorName;
-		var url = request.url;
+chrome.runtime.onMessage.addListener(
+    function(request, sender, callback) {
+        if (request.action == "xhttp") {
+             var xhttp = new XMLHttpRequest();
+             var method = request.method ? request.method.toUpperCase() : 'GET';
 
-		var data = {
-			responseXML: "",
-			professorName: professorName,
-			url: url
-		};
+     xhttp.onload = function() {
+        console.log("Loaded URl: " + request.url);
+        console.log("Professor: " + request.professor);
+        callback({
+             response: xhttp.responseText,
+             professorURL: request.link,
+             professorIndex: request.index
+        });
 
-		xhttp.onload = function()
-		{
-			data.responseXML = this.responseText;
-			callback(data);
-		};
+        console.log("Does callback work?");
+    };
 
-		xhttp.onerror = function()
-		{
-			callback();
-		};
+        xhttp.onerror = function() {
+            // callback to clean up the communication port.
+            console.log("error");
+            callback();
+        };
 
-		xhttp.open(method, request.url, true);
+        console.log("Attempting to open URL: " + request.url);
+        xhttp.open(method, request.url, true);
 
-		if(method == 'POST')
-			xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        if (method == 'POST') {
+            xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+         }
 
-		xhttp.send();
+        console.log("do we get after post?");
 
-		return true;
-	}
-});
+        xhttp.send(request.data);
 
+        console.log("4HEAD?");
+
+        return true; // prevents the callback from being called too early on return
+
+        }
+    }
+);
